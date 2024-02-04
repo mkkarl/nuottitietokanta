5d3bb7796805c3efd0439dc9c0a5d4028e49cd9b
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2024-02-04 22:56:09 +0200

    Add login, logout, register (not working)

---
+++ routes.py
@@ -0,0 +1,42 @@
+from app import app
+from flask import redirect, render_template, request, session
+import users
+
+@app.route("/")
+def index():
+    user_id = users.user_id()
+    if user_id == 0:
+        return redirect("/login")
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Väärä tunnus tai salasana")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Salasanat eroavat")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Rekisteröinti ei onnistunut")
