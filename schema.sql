005a14c1c145675567cb2d727acefb5c2016982b
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2024-02-18 17:47:12 +0200

    Add table 'sheet' to db

--- schema.sql
+++ schema.sql
@@ -9,3 +9,14 @@ CREATE TABLE sheet_collection (
     name TEXT,
     type TEXT
 );
+
+CREATE TABLE sheet (
+    id SERIAL PRIMARY KEY,
+    collection_id INTEGER REFERENCES sheet_collection,
+    name TEXT,
+    composer TEXT,
+    writer TEXT,
+    arranger TEXT,
+    creator TEXT,
+    artist TEXT
+);
